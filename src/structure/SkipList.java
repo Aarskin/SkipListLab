1e07ffab92fe119cca544c8e4b9d6d1f6bd61b9a
mwiemer <mwiemer@outlook.com>
2014-04-18 16:35:13 -0500

    Fleshed out SkipNode

--- src/structure/SkipList.java
+++ src/structure/SkipList.java
@@ -1,11 +1,32 @@
 package structure;
 
-public class SkipList<T extends Comparable<T>> {
-	SkipNode<T> root, tail;
+public class SkipList<T extends Comparable<T>> 
+{
+	private SkipNode<T> head, tail;
 	
 	public SkipList()
 	{
-		root = new SkipNode<T>();
-		tail = new SkipNode<T>();
+		head = new SkipNode<T>("HEAD");
+		tail = new SkipNode<T>("TAIL");
+		
+		head.hlink(tail);
+	}
+	
+	public void insert(T val)
+	{
+		SkipNode<T> A = head;
+		SkipNode<T> B = tail;
+		
+		
+	}
+	
+	public boolean delete()
+	{
+		return false;
+	}
+	
+	public T find()
+	{
+		return null;
 	}
 }
--- src/structure/SkipNode.java
+++ src/structure/SkipNode.java
@@ -1,12 +1,70 @@
 package structure;
 
-public class SkipNode<T extends Comparable<T>> {
-	SkipNode<T> left, right, up, down;
+public class SkipNode<T extends Comparable<T>> 
+{
+	Boolean isHead, isTail; // Null if this is a typical node
 	
-	T value;
+	SkipNode<T> left;	// Null for HEAD
+	SkipNode<T> right;	// Null for TAIL
+	SkipNode<T> up;		// Null for top row
+	SkipNode<T> down;	// Null for bottom row
+	T value; 
 	
-	public SkipNode()
+	/* Only assign booleans on a terminal node */
+	public SkipNode(String terminal)
 	{
-		
+		if(terminal.equals("HEAD"))
+		{
+			isHead = true;
+		}
+		else if(terminal.equals("TAIL"))
+		{
+			isTail = true;
+		}
+		else
+		{
+			System.err.println("Invalid Identifier -- HEAD | TAIL");
+		}
 	}
+	
+	/* Assign neighbors and value for a typical node */
+	public SkipNode(SkipNode<T> left, SkipNode<T> right, SkipNode<T> up,
+					SkipNode<T> down, T value)
+	{
+		this.left = left;
+		this.right = right;
+		this.up = up;
+		this.down = down;
+		
+		this.value = value;
+	}
+
+	/* Creates an edge between this node and the one to the right */
+	public void linkRight(SkipNode<T> other)
+	{
+		right = other;
+		other.left = this; 
+	}
+	
+	/* Creates an edge between this node and the one to the left */
+	public void linkLeft(SkipNode<T> other)
+	{
+		left = other;
+		other.right = this; 
+	}
+	
+	/* Creates an edge between this node and the one to the up */
+	public void linkUp(SkipNode<T> other)
+	{
+		up = other;
+		other.up = this; 
+	}
+	
+	/* Creates an edge between this node and the one to the down */
+	public void linkDown(SkipNode<T> other)
+	{
+		down = other;
+		other.down = this; 
+	}
+
 }
